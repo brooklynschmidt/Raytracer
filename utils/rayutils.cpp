456cce1a35b1e5366804904f802a69e6fdad9dff
brooklynschmidt <schmidt.bro@northeastern.edu>
2025-07-08 06:11:33 -0400

    added sphere

--- utils/rayutils.cpp
+++ utils/rayutils.cpp
@@ -3,6 +3,11 @@
 #include <rayutils.h>
 
 Color ray_color(const Ray& r) {
+
+    if (hit_sphere(Point(0, 0, -1), 0.5, r)) {
+        return Color(1, 0, 0);
+    }
+    
     Vec3 unit_direction = unit(r.direction());
     double lerp = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - lerp) * Color(1.0, 1.0, 1.0) + lerp * Color(0.5, 0.7, 1.0);
@@ -18,4 +23,13 @@ void write_color(std::ostream& out, const Color& pixel_color) {
     int bbyte = int(255.999 * b);
 
     out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
+
+bool hit_sphere(const Point& center, double radius, const Ray& r) {
+    Vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
 }
