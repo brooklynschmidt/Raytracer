7037349f5632235983177cebb6bc1acccaab488e
brooklynschmidt <schmidt.bro@northeastern.edu>
2025-10-12 00:01:15 -0400

    implemented fuzzy reflections

--- include/material.h
+++ include/material.h
@@ -37,16 +37,24 @@ class Lambertian : public Material {
 
 class Metal : public Material {
     public:
-        Metal(const Color& albedo) : albedo(albedo) {}
+        Metal(const Color& albedo, double fuzz) : albedo(albedo), fuzz(fuzz < 1 ? fuzz : 1) {}
 
+        /* Randomize the reflected direction using a small sphere and choosing a new endpoint for the ray
+        We use a random point from the surface of a sphere centered on the original endpoint, scaled by 
+        the fuzz factor. The bigger the fuzz sphere, the fuzzier the reflections. 
+        Absorb grazing rays or big spheres that scatter below the surface.
+        The fuzz sphere is scaled in relation to the reflection vector, thus a normalization is required. 
+        */
         bool scatter(const Ray& r_in, const hit_record& rec, Color& attenuation, Ray& scattered) const override {
             Vec3 reflected = reflect(r_in.direction(), rec.normal);
+            reflected = unit(reflected) + (fuzz * random_unit_vector());
             scattered = Ray(rec.p, reflected);
             attenuation = albedo;
-            return true;
+            return (dot(scattered.direction(), rec.normal) > 0);
         }
     private:
         Color albedo;
+        double fuzz;
 };
 
 #endif
